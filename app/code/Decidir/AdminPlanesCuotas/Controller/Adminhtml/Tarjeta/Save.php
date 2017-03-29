3f543249b75548285900a941fdf6b07b99f7303b
Lucas Gonzalez <lucasg@nei-si.com>
2017-03-29 03:33:30 -0300

    Corrección para funcionamiento de administrador de planes de pago

--- app/code/Decidir/AdminPlanesCuotas/Controller/Adminhtml/Tarjeta/Save.php
+++ app/code/Decidir/AdminPlanesCuotas/Controller/Adminhtml/Tarjeta/Save.php
@@ -66,6 +66,6 @@ class Save extends \Magento\Backend\App\Action
      */
     protected function _isAllowed()
     {
-        return $this->_authorization->isAllowed('Decidir_AdminPlanesCuotas::tarjetas);
+        return $this->_authorization->isAllowed('Decidir_AdminPlanesCuotas::tarjetas');
     }
 }
